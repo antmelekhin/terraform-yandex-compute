28d7d414de6a71310c24c1555e5f91d6e510a41d
Melekhin Anton <antmelekhin@gmail.com>
2023-03-30 23:23:30 +0400

    fix tflint warnings

--- examples/instance/secondary-disk/main.tf
+++ examples/instance/secondary-disk/main.tf
@@ -94,7 +94,7 @@ module "instances_multiple_secondary_disks" {
 
   boot_disk_image_id = data.yandex_compute_image.ubuntu_2004.id
   secondary_disk_ids = [
-    element(yandex_compute_disk.data_one.*.id, count.index),
-    element(yandex_compute_disk.data_two.*.id, count.index),
+    element(yandex_compute_disk.data_one[*].id, count.index),
+    element(yandex_compute_disk.data_two[*].id, count.index),
   ]
 }
